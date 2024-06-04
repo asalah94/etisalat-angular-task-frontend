42496f32c8e4177e5dab04ca30300034a538a162
ahmsalah <ahmsalah.elsayed@gmail.com>
2024-06-04 14:30:40 +0300

    Initial commit

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,41 @@
+
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { User } from '../user.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[] | undefined;
+
+  constructor(private userService: UserService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.userService.getUserList().subscribe(data => {
+      this.users = data;
+    });
+  }
+
+  updateUser(id: number) {
+    this.router.navigate(['update-user', id]);
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe(data => {
+      console.log(data);
+      this.getUsers();
+    });
+  }
+}
+
